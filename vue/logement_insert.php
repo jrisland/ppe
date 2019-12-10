ad91cac670eb1dd7e88027cd727741d7aa9875bf
sendinee <48397577+sendinee@users.noreply.github.com>
2019-12-10 13:18:29 +0100

    Create logement_insert.php

---
+++ vue/logement_insert.php
@@ -0,0 +1,35 @@
+<center>
+  <form method="post" action="">
+    <div class="form-group">
+      <label style="color: #ecf0f1;" for="insert">Id Service:</label>
+        <input class="form-control" type="text" name="idservice">
+    </div>
+    <div class="form-group">
+      <label style="color: #ecf0f1;" for="insert">Adresse:</label>
+        <input class="form-control" type="text" name="adresse">
+    </div>
+    <div class="form-group">
+      <label style="color: #ecf0f1;" for="insert">Date de debut:</label>
+        <input class="form-control" type="date" name="datedebut"></td>
+    </div>
+
+      <div class="form-group">
+          <label style="color: #ecf0f1;" for="insert">Date de fin:</label>
+          <input class="form-control" type="date" name="datefin">
+      </div>
+      <div class="form-group">
+          <label style="color: #ecf0f1;" for="insert">Propreté:</label>
+          <input class="form-control" type="text" name="nettoyage">
+      </div>
+      <div class="form-group>
+            <label style="color: #ecf0f1;" for="insert">Surface:</label>
+            <input class="form-control" type="number" name="surface">
+      </div>
+      <div class="form-group>
+            <label style="color: #ecf0f1;" for="insert">Prix:</label>
+            <input class="form-control" type="number" name="prix">
+      </div>
+      <button class="btn btn-primary" type="reset" name="Annuler" value="Annuler">Annuler</button>
+      <button class="btn btn-primary" type="submit" name="Valider" value="Valider">Valider</button>
+  </form>
+</center>
