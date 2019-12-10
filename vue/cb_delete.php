f49cbf95d8ce0175f00185448f1e26c878257adb
sendinee <48397577+sendinee@users.noreply.github.com>
2019-12-10 12:19:16 +0100

    Create cb_delete.php

---
+++ vue/cb_delete.php
@@ -0,0 +1,10 @@
+<h2 style="color: white;"> Suppression de la CB </h2>
+<br>
+<form method="post" action="">
+    <div  class="form-group">
+        <label for="suppression">Id CB :</label>
+        <input class="form-control" type="text" name="idcb" size="5">
+    </div>
+    <button class="btn btn-primary" type="submit" name="Supprimer" value="Supprimer"> Supprimer </button>
+</form>
+</br>
