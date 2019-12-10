d85e38790246cdcb56eadbe7609d35920add966b
sendinee <48397577+sendinee@users.noreply.github.com>
2019-12-10 12:18:51 +0100

    Create cb_insert.php

---
+++ vue/cb_insert.php
@@ -0,0 +1,31 @@
+<center>
+  <form method="post" action="">
+    <div class="form-group">
+      <label style="color: #ecf0f1;" for="insert">id cb:</label>
+        <input class="form-control" type="text" name="idcb">
+    </div>
+    <div class="form-group">
+      <label style="color: #ecf0f1;" for="insert">Nom:</label>
+        <input class="form-control" type="text" name="nom">
+    </div>
+    <div class="form-group">
+      <label style="color: #ecf0f1;" for="insert">Numero:</label>
+        <input class="form-control" type="text" name="numero"></td>
+    </div>
+
+      <div class="form-group">
+          <label style="color: #ecf0f1;" for="insert">Date de validité:</label>
+          <input class="form-control" type="date" name="datevalidite">
+      </div>
+      <div class="form-group">
+          <label style="color: #ecf0f1;" for="insert">Crypto monnaie:</label>
+          <input class="form-control" type="text" name="crypto">
+      </div>
+	  <div class="form-group">
+          <label style="color: #ecf0f1;" for="insert">Id Client:</label>
+          <input class="form-control" type="text" name="idclient">
+      </div>
+      <button class="btn btn-primary" type="reset" name="Annuler" value="Annuler">Annuler</button>
+      <button class="btn btn-primary" type="submit" name="Valider" value="Valider">Valider</button>
+  </form>
+</center>
