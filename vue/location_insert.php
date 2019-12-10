bf2f8f28fc2942d66f89ccfba6a083be683a2953
sendinee <48397577+sendinee@users.noreply.github.com>
2019-12-10 13:17:47 +0100

    Create location_insert.php

---
+++ vue/location_insert.php
@@ -0,0 +1,34 @@
+<center>
+  <form method="post" action="">
+        <div class="form-group">
+            <label style="color: #ecf0f1;" for="insert">Immatriculation</label>
+            <input class="form-control" type="text" name="immatriculation">
+        </div>
+        <div class="form-group">
+            <label style="color: #ecf0f1;" for="insert">Numéro de parking:</label>
+            <input class="form-control" type="number" name="n_parking">
+        </div>
+        <div class="form-group">
+            <label style="color: #ecf0f1;" for="insert">Satut:</label>
+            <input class="form-control" type="text" name="statut"></td>
+        </div>
+       <div class="form-group">
+           <label style="color: #ecf0f1;" for="insert">Date de Début:</label>
+           <input class="form-control" type="date" name="datedebut">
+       </div>
+       <div class="form-group">
+           <label style="color: #ecf0f1;" for="insert">Date de Fin:</label>
+           <input class="form-control" type="date" name="datefin">
+       </div>
+       <div class="form-group">
+           <label style="color: #ecf0f1;" for="insert">Surface:</label>
+           <input class="form-control" type="text" name="surface">
+       </div>
+       <div class="form-group">
+           <label style="color: #ecf0f1;" for="insert">Prix:</label>
+           <input class="form-control" type="text" name="prix">
+       </div>
+      <button class="btn btn-primary" type="reset" name="Annuler" value="Annuler">Annuler</button>
+      <button class="btn btn-primary" type="submit" name="Valider" value="Valider">Valider</button>
+  </form>
+</center>
