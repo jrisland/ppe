609edb55b02bd1250ef14d58d268f1c04f2b3e40
sendinee <48397577+sendinee@users.noreply.github.com>
2019-12-16 13:38:42 +0100

    Update menu_site.php

--- menu_site.php
+++ menu_site.php
@@ -2,25 +2,34 @@
     <?php
     require("vue/header.php")
     ?>
-    <link rel="stylesheet" href="./css/gestionobjet.css">
-    <link rel="stylesheet" href="./css/navbar.css">
+    <link rel="stylesheet" href="./css/index.css">
 </head>
 <header>
-<nav class="navbar navbar-expand-lg navbar-light bg-light" style="height:6%;">
-  <a class="navbar-brand" href="menu_site.php?page=1"><img class="logo" src="./vue/effray.png"> </a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="height:162%;">
+  <a class="navbar-brand" href="menu_site.php?page=1"><img class="logo" src="./image/corsair.png"> </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="menu_site.php?page=2">Objets</a>
+        <a class="nav-link" style="color: black;" href="menu_site.php?page=2">Vols</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="menu_site.php?page=3">Pricing</a>
+        <a class="nav-link" style="color: black;" href="menu_site.php?page=3">Hébergement</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" style="color: black;" href="menu_site.php?page=4">Location</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" style="color: black;"href="menu_site.php?page=5">Promotion</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" style="color: black;"href="menu_site.php?page=6">Carte Bleu</a>
       </li>
     </ul>
-    <button class="btn btn-danger my-2 my-sm-0"><a id="deco" style="color: black;" href="menu_site.php?page=4">Déconnexion</a></button>
+    <button class="btn btn-danger my-2 my-sm-0"><a id="deco" style="color: black;" href="menu_site.php?page=7">Compte Client</a></button>
+    <button class="btn btn-danger my-2 my-sm-0"><a id="deco" style="color: black;" href="menu_site.php?page=8">Déconnexion</a></button>
   </div>
 </nav>
  
@@ -36,13 +45,21 @@
           $page = 0;
         }
         switch($page) {
-          case 1 : require_once ("gestionproduit.php");
+          case 1 : require_once ("./vue/reserver.php");
           break;
-          case 2 : require_once ("gestionobjet.php");
+          case 2 : require_once ("gestionvol.php");
           break;
-          case 3 : require_once ("gestionutilisateur.php");
+          case 3 : require_once ("gestionlogement.php");
           break;
-          case 4 : require_once ("model/deconnexion.php");
+          case 4 : require_once ("gestionlocation.php");
+          break;
+          case 5 : require_once ("gestionpromotion.php");
+          break;
+          case 6 : require_once ("gestioncb.php");
+          break;
+          case 7 : require_once ("gestionclient.php");
+          break;
+          case 8 : require_once ("model/deconnexion.php");
           break;
         }
        ?>
